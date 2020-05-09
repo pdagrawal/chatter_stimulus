f11027af1a43bba130a19b7cd67a1421178990b6
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-05-10 03:05:05 +0530

    working real time blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  include CableReady::Broadcaster
+
   def index
-    @posts = Post.all.order(creted_at: :desc)
+    @posts = Post.order(id: :desc)
     @post = Post.new
   end
 
@@ -8,6 +10,12 @@ class PostsController < ApplicationController
     post = Post.new(post_params)
     post.username = 'Pradeep'
     post.save
+    cable_ready['timeline'].insert_adjacent_html(
+      selector: '#timeline',
+      position: 'afterbegin',
+      html: render_to_string(partial: 'post', locals: { post: post })
+    )
+    cable_ready.broadcast
     redirect_to posts_path
   end
 
